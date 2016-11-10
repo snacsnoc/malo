448ab37e6d9d8b469871126778fef6f782e4e2fd
Easton Elliott <easton@geekness.eu>
2016-11-10 14:33:10 -0800

    Remove reddit functions, add github.com parsing, remove shibabot function, add !lenny and !rms command

--- commands.inc.php
+++ commands.inc.php
@@ -3,12 +3,8 @@
 //Goddamnit this code is terrible
 //....but it works
 
-
-use \RedditApiClient\Reddit;
-
-
-
 $redis = new Redis();
+
 
 //Check if we can connect to redis
 // port 6379 by default
@@ -16,17 +12,14 @@ if (false == $redis->connect($config['redis_server'])) {
     die('Redis server down. Check configuration!');
 }
 
-
-
 $client = new Google_Client();
 $client->setApplicationName("Client_Library_Examples");
 
 $client->setDeveloperKey($config['google_services_apikey']);
 
 
-
 //Version 
-$version = "malo IRC bot version 1.914 by snacsnoc <easton@geekness.eu>";
+$version = "malo IRC bot version 1.915 by snacsnoc <easton@geekness.eu>";
 
 //Check if the user is in the banlist
 if (false == in_array($nickc[1], $ban_list)) {
@@ -45,7 +38,7 @@ if (false == in_array($nickc[1], $ban_list)) {
         $search_key = trim($ex[4]);
     }
 
-    if (true == preg_match_all("%(?:.*)?(?:s)?(?:.*)(?s)(?:http(?:s)://)?(?:www.)?(?:youtube.com|youtu.be)/(?:watch\?v=)?(.+)?(?)%", $search_key, $m)) {
+    if (true == preg_match_all("%(?:.*)?(?:s)?(?:.*)(?s)(?:http(?:s)://)?(?:www.)?(?:youtube.com|youtu.be)/(?:watch\?v=)?(.{11})?%", $search_key, $m)) {
 
         /*
         This should not work:
@@ -56,7 +49,7 @@ if (false == in_array($nickc[1], $ban_list)) {
         http://www.youtube.com/watch?v=uQX4GuSXYLM
         */
 
-        if (!empty($m['0'])) {
+        if (!empty($m[0])) {
             if (preg_match("#(\?|&)#si", $m[1][1])) {
                 parse_str($m['1'], $vars);
                 
@@ -67,7 +60,7 @@ if (false == in_array($nickc[1], $ban_list)) {
                 //Maybe test against preg_match("#^([A-Za-z0-9\-\_]+?)#si", $m['1'])...
                 $youtube_video_key = $m[1][0];
             }
-            
+
             //Validate that video key is 11 characters
             if (strlen($youtube_video_key) == 11) {
 
@@ -100,6 +93,31 @@ if (false == in_array($nickc[1], $ban_list)) {
             }
         }
     }
+
+    //Parse github.com URLs
+     if (true == preg_match_all("%(?:http(?:s):\/\/)?(?:www.)?github.com\/(.*)\/(.*)%", $ex[3], $match)) {
+
+        if($match){
+            $repo_to_get = trim($match[1][0]."/".$match[2][0]);
+            $rss_feed    = "https://github.com/$repo_to_get/commits/master.atom";
+            $feed        = simplexml_load_file($rss_feed);
+            if ($feed) {
+                
+                $link  = $feed->entry[0]->link->attributes();
+                $short_url  = make_bitly_url($link['href'], $config['bitly_username'], $config['bitly_apikey'], 'xml');
+               
+                $title = trim($feed->entry[0]->title);
+                
+                #$latest_commit = "$title [commit: $short_url]";
+                fputs($socket, "PRIVMSG " . $config['chan'] . " :Latest commit: $title [commit: $short_url]\r\n");
+            } else {
+                fputs($socket, "PRIVMSG " . $config['chan'] . " :Unable to get latest activity :() \r\n");
+            }
+        }else{
+            echo "No match on github URL\r\n";
+        }
+      }  
+
     if ($ex[1] == "KICK") {
         fputs($socket, "JOIN " . $config['chan'] . "\r\n");
     }
@@ -124,7 +142,6 @@ if (false == in_array($nickc[1], $ban_list)) {
     //Standard period and exclaimation mark prefixed commands
     switch (strtolower(rtrim($rawcmd[1]))) {
         
-        
         case ".say":
             fputs($socket, "PRIVMSG " . $config['chan'] . " :$args \r\n");
             break;
@@ -136,7 +153,6 @@ if (false == in_array($nickc[1], $ban_list)) {
         //searches youtube for keyword
         case ".yt":
             
-
             //See https://developers.google.com/youtube/v3/docs/videos
             $youtube = new Google_Service_YouTube($client);
 
@@ -183,129 +199,6 @@ if (false == in_array($nickc[1], $ban_list)) {
             }else{
                 fputs($socket, "PRIVMSG " . $config['chan'] . " :$nickc[1]: No results :( \r\n");
             }
-            break;
-        
-        
-        //Reddit commands
-        case ".reddit":
-            
-            //Separeate commands my spaces
-            $reddit_command = explode(" ", $args);
-            
-            if (null == $args) {
-                fputs($socket, "PRIVMSG " . $nickc[1] . " :Usage:\r\n");
-                fputs($socket, "PRIVMSG " . $nickc[1] . " :get <subreddit>     Get top link in a given subreddit\r\n");
-                fputs($socket, "PRIVMSG " . $nickc[1] . " :comment <thread_id> <comment text>      Comment on a thread given the ID\r\n");
-                fputs($socket, "PRIVMSG " . $nickc[1] . " :vote <thread_id> <up/down>     Upvote/downvote a post given the ID\r\n");
-                fputs($socket, "PRIVMSG " . $nickc[1] . " :info     Get current Reddit bot info\r\n");
-                break;
-            }
-            //Log in
-            $reddit = new Reddit($config['reddit_username'], $config['reddit_password']);
-            
-            
-            
-            
-            switch (trim($reddit_command[0])) {
-                //Get info about logged in user
-                case "info":
-                    
-                    $response = $reddit->sendRequest("GET", "http://www.reddit.com/api/me.json");
-                    if (true == $response['data']['has_mail']) {
-                        $response['data']['has_mail'] = "yes";
-                    } else {
-                        $response['data']['has_mail'] = "no";
-                    }
-                    fputs($socket, "PRIVMSG " . $config['chan'] . " : [Username: " . $response['data']['name'] . "] [Comment karma: " . $response['data']['comment_karma'] . "] [Has mail: " . $response['data']['has_mail'] . "] \r\n");
-                    break;
-                
-                //Retrieves the top link in a given subreddit
-                case "get":
-                    try {
-                        $subreddit_links = $reddit->getLinksBySubreddit(trim($reddit_command[1]));
-                        
-                        $topLink          = $subreddit_links[0];
-                        $reddit_thread_id = $topLink->getId();
-                        $upvotes          = $topLink->getUpvotes();
-                        $downvotes        = $topLink->getDownvotes();
-                        
-                        //Fix some problems with titles of threads
-                        $title = trim($topLink->getTitle());
-                        
-                        if ($topLink->isSelfPost()) {
-                            $self_text = $topLink->getSelfText();
-                            $self_text = substr($self_text, 0, 50);
-                            $self_text = "[$self_text...]";
-                        } else {
-                            $self_text = '';
-                        }
-                        
-                        //To save some screen space
-                        $short_url = make_bitly_url($topLink->getUrl(), $config['bitly_username'], $config['bitly_apikey'], 'xml');
-                        
-                        
-                        fputs($socket, "PRIVMSG " . $config['chan'] . " :$nickc[1]: " . $title . " " . $short_url . " [$upvotes/$downvotes] [Thread ID:$reddit_thread_id] $self_text\n");
-                        
-                        //Unset it so it doesn't reappear again
-                        unset($self_text);
-                    }
-                    catch (Exception $e) {
-                        
-                        fputs($socket, "PRIVMSG " . $config['chan'] . " :$nickc[1]: " . $e->getMessage() . " \r\n");
-                    }
-                    break;
-                
-                //Posts a comment based on the thread ID
-                case "comment":
-                    
-                    $thread_id = $reddit_command[1];
-                    
-                    //Separate the comment from the command
-                    $text         = explode("comment $thread_id", $args);
-                    $comment_text = $text[1];
-                    
-                    //Get Reddit link and post comment on root  
-                    $subreddit_post = $reddit->getLink($thread_id);
-                    
-                    if (true == $subreddit_post->reply($comment_text)) {
-                        fputs($socket, "PRIVMSG " . $config['chan'] . " :$nickc[1]: comment confirmed \r\n");
-                    } else {
-                        fputs($socket, "PRIVMSG " . $config['chan'] . " :$nickc[1]: error: cannot comment :( \r\n");
-                    }
-                    
-                    break;
-                //Upvotes or downvotes a thread (link) given the thread ID
-                case "vote":
-                    $thread_id = $reddit_command[1];
-                    
-                    //Separate up/down from the command
-                    $vote = explode("vote $thread_id", $args);
-                    
-                    //Maybe it's Pidgin, but there seems to be a whitespace issue
-                    $vote_direction = trim($vote[1]);
-                    
-                    //Convert the words to an integer! Yay!   
-                    if ($vote_direction == "up") {
-                        $vote_direction = 1;
-                    } elseif ($vote_direction == "down") {
-                        $vote_direction = -1;
-                    } else {
-                        $vote_direction = 0;
-                    }
-                    
-                    $set_vote = $reddit->getLink($thread_id);
-                    if (true == $set_vote) {
-                        //If the vote was successful, return it to the user
-                        if ($set_vote->vote($vote_direction)) {
-                            
-                            fputs($socket, "PRIVMSG " . $config['chan'] . " :$nickc[1]: vote confirmed \r\n");
-                        } else {
-                            fputs($socket, "PRIVMSG " . $config['chan'] . " :$nickc[1]: cannot vote :( \r\n");
-                        }
-                    }
-                    break;
-            }
-            
             break;
         
         //random noun
@@ -473,15 +366,9 @@ if (false == in_array($nickc[1], $ban_list)) {
             fputs($socket, "PRIVMSG " . $config['chan'] . " :" . strtoupper($nickc[1]) . chr(3) . chr(53) . chr(44) . chr(49) . chr(51) . " PARTY TIME! WOOT WOOT!\r\n");
             break;
         
-        
+        //Get the latest recipe
         case '.sonatia':
-            /*
-             * http://feeds.feedburner.com/DinnerTonight
-             * http://www.taste.com.au/feeds/latest+recipes.xml
-             * http://darinacooking.com/category/course/dessert/feed/
-             * http://www.food.com/rssapi.do?page_type=28&slug=desserts
-             */
-            $food_xml = simplexml_load_file('http://www.food.com/rssapi.do?page_type=28&slug=desserts');
+            $food_xml = simplexml_load_file('http://www.taste.com.au/feeds/latest+recipes.xml');
             if ($food_xml) {
                 $title       = $food_xml->channel->item->title;
                 $description = $food_xml->channel->item->description;
@@ -693,8 +580,8 @@ if (false == in_array($nickc[1], $ban_list)) {
         case ".w":
             $weather_command = explode(" ", $args);
             
-            $forecast = new ForecastIO($config['forecast.io_apikey'], 'auto', 'en');
-            
+            $forecast = new ForecastIO($config['forecast.io_apikey'], 'ca', 'en');
+
             switch (trim($weather_command[0])) {
                 
                 case 'set':
@@ -743,7 +630,8 @@ if (false == in_array($nickc[1], $ban_list)) {
                         //If short_name is US then we use F, otherwise C
                         switch ($output->results[0]->address_components[3]->short_name){
                             case "US":
-                                $units = "°F";
+                                #$units = "°F";
+                                $units = "°C"; 
                                 break;
 
                             default:
@@ -782,7 +670,8 @@ if (false == in_array($nickc[1], $ban_list)) {
                     //If short_name is US then we use F, otherwise C
                     switch ($output->results[0]->address_components[3]->short_name){
                         case "US":
-                            $units = "°F";
+                            #$units = "°F";
+                        $units = "°C"; 
                             break;
 
                         default:
@@ -842,13 +731,33 @@ if (false == in_array($nickc[1], $ban_list)) {
             $random = rand(0, count($link) - 1);
             fputs($socket, "PRIVMSG " . $config['chan'] . " :$nickc[1]: " . "$link[$random]\r\n");
             break;
-        
+
+        case "!rms":
+            $link[] = "Thanks to Mr. Gates, we now know that an open Internet with protocols anyone can implement is communism; it was set up by that famous communist agent, the US Department of Defense.";
+            $link[] = "People said I should accept the world. Bullshit! I don't accept the world.";
+            $link[] = "Four species of penguins that breed in Antartica are endangered by global warming.";
+            $link[] = "C++ is a badly designed and ugly language. It would be a shame to use it in Emacs.";
+            $link[] = "If the users don't control the program, the program controls the users. With proprietary software, there is always some entity, the owner of the program, that controls the program—and through it, exercises power over its users. A nonfree program is a yoke, an instrument of unjust power.";
+            $link[] = "Any time I connect to a website other than Wikipedia, it's through Tor.";
+            $link[] = "People sometimes ask me if it is a sin in the Church of Emacs to use vi. Using a free version of vi is not a sin; it is a penance. So happy hacking. ";
+            $link[] = "GNU is not in the public domain. Everyone will be permitted to modify and redistribute GNU, but no distributor will be allowed to restrict its further redistribution. That is to say, proprietary modifications will not be allowed. I want to make sure that all versions of GNU remain free.";
+            $link[] = "Writing non-free software is not an ethically legitimate activity, so if people who do this run into trouble, that's good! All businesses based on non-free software ought to fail, and the sooner the better.";
+            $link[] = "Very ironic things have happened, but nothing to match this — giving the Linus Torvalds Award to the Free Software Foundation is sort of like giving the Han Solo Award to the Rebel Fleet.";
+            $link[] = "Today many people are switching to free software for purely practical reasons. That is good, as far as it goes, but that isn't all we need to do! Attracting users to free software is not the whole job, just the first step.";
+            $link[] = "I could have made money this way, and perhaps amused myself writing code. But I knew that at the end of my career, I would look back on years of building walls to divide people, and feel I had spent my life making the world a worse place.";
+            $random = rand(0, count($link) - 1);
+            fputs($socket, "PRIVMSG " . $config['chan'] . " :$nickc[1]: " . "$link[$random]\r\n");
+            break;        
         
         //There needs to a better way to list commands         
         case ".help":
-            fputs($socket, "PRIVMSG " . $nickc[1] . " :All commands are here: https://pasteros.io/5535791ad06e0 \r\n");
+            fputs($socket, "PRIVMSG " . $nickc[1] . " :All commands are here: https://pasteros.io/5822582913ee8/raw \r\n");
             break;
-        
+  
+        case "!lenny":
+            fputs($socket, "PRIVMSG " . $config['chan']. " :( ͡° ͜ʖ ͡°) \r\n");
+            break;       
+
         ##BOT/SYS COMMANDS##
         case ".uptime":
             if ($ex[0] == $config['admin']) {
@@ -915,7 +824,6 @@ if (false == in_array($nickc[1], $ban_list)) {
                 $link  = $feed->entry[0]->link->attributes();
                 $title = trim($feed->entry[0]->title);
                 
-                
                 $short_url     = make_bitly_url($link['href'], $config['bitly_username'], $config['bitly_apikey'], 'xml');
                 $latest_commit = "$title [commit: $short_url]";
             } else {
@@ -924,25 +832,7 @@ if (false == in_array($nickc[1], $ban_list)) {
             
             fputs($socket, "PRIVMSG " . $config['chan'] . " :https://github.com/snacsnoc/snacklinux Latest commit: $latest_commit\r\n");
             break;
-        
-        case ".shibabot":
-            $rss_feed = 'https://github.com/notori0us/shibabot/commits/master.atom';
-            $feed     = simplexml_load_file($rss_feed);
-            if ($feed) {
-                
-                $link  = $feed->entry[0]->link->attributes();
-                $title = trim($feed->entry[0]->title);
-                
-                
-                $short_url     = make_bitly_url($link['href'], $config['bitly_username'], $config['bitly_apikey'], 'xml');
-                $latest_commit = "$title [commit: $short_url]";
-            } else {
-                return false;
-            }
-            
-            fputs($socket, "PRIVMSG " . $config['chan'] . " :https://github.com/notori0us/shibabot Latest commit: $latest_commit\r\n");
-            break;
-        
+             
         
         //This doesn't work
         case ".restart":
